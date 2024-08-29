f53b370068a5e0debd5629d0bbe56c9ee6041178
qorganbek <dimashqorganbek@gmail.com>
2024-08-29 16:44:15 +0500

    rds basic settings

--- main.tf
+++ main.tf
@@ -1,81 +1,33 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
 provider "aws" {
-  region = "us-east-2"
+  region = "us-east-1"
 }
 
-provider "random" {}
-
-data "aws_availability_zones" "available" {}
-
-resource "random_pet" "random" {}
-
-module "vpc" {
-  source  = "terraform-aws-modules/vpc/aws"
-  version = "2.77.0"
-
-  name                 = "${random_pet.random.id}-education"
-  cidr                 = "10.0.0.0/16"
-  azs                  = data.aws_availability_zones.available.names
-  public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-}
-
-resource "aws_db_subnet_group" "education" {
-  name       = "${random_pet.random.id}-education"
-  subnet_ids = module.vpc.public_subnets
-
-  tags = {
-    Name = "${random_pet.random.id} Education"
-  }
-}
-
-resource "aws_security_group" "rds" {
-  name   = "${random_pet.random.id}-education_rds"
-  vpc_id = module.vpc.vpc_id
-
-  ingress {
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-    cidr_blocks = ["192.80.0.0/16"]
-  }
-
-  egress {
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "${random_pet.random.id}-education_rds"
-  }
-}
-
-resource "aws_db_parameter_group" "education" {
-  name   = "${random_pet.random.id}-education"
-  family = "postgres15"
-
-  parameter {
-    name  = "log_connections"
-    value = "1"
-  }
-}
-
-resource "aws_db_instance" "education" {
-  identifier             = "${var.db_name}-${random_pet.random.id}"
-  instance_class         = "db.t3.micro"
-  allocated_storage      = 5
+resource "aws_db_instance" "rds" {
+  identifier             = var.db_identifier
+  instance_class         = var.size
+  allocated_storage      = var.allocated_storage
+  max_allocated_storage  = var.max_allocated_storage
+  storage_type           = var.storage_type
   engine                 = "postgres"
-  engine_version         = "15.6"
+  engine_version         = "15.4"
+  db_name                = var.db_name
   username               = var.db_username
   password               = var.db_password
-  db_subnet_group_name   = aws_db_subnet_group.education.name
-  vpc_security_group_ids = [aws_security_group.rds.id]
-  parameter_group_name   = aws_db_parameter_group.education.name
-  publicly_accessible    = true
-  skip_final_snapshot    = true
+  vpc_security_group_ids = var.security_group_ids
+  publicly_accessible    = false
+  multi_az               = var.multi_az
+
+  auto_minor_version_upgrade            = false
+  deletion_protection                   = true
+  apply_immediately                     = true
+  performance_insights_enabled          = var.performance_insights
+  performance_insights_retention_period = 7
+  performance_insights_kms_key_id       = var.kms_key_id
+
+
+  backup_retention_period = 7
+  copy_tags_to_snapshot   = true
+  backup_window           = var.backup_window
+  storage_encrypted       = true
+  skip_final_snapshot     = true
 }
